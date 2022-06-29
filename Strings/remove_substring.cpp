690e4eecb02f6c70a70885186b2dbf635d3492f6
Saksham Srivastava <saksham16dec@gmail.com>
2022-06-29 09:45:18 +0530

    Removing substring ina string

---
+++ Strings/remove_substring.cpp
@@ -0,0 +1,20 @@
+// remove substring in a string
+#include <iostream>
+#include <vector>
+using namespace std;
+string removeOccurrences(string s, string part)
+{
+    while (s.length() != 0 && s.find(part) < s.length())
+    {
+        s.erase(s.find(part), part.length());
+    }
+    return s;
+}
+int main()
+{
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    cout << removeOccurrences(s, part) << endl;
+    return 0;
+}
+// Question Link :https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
